80b38f43ebcb8a455eaa1b7c6989be286a22cb6b
Devlin Brush <devlinbrush@gmail.com>
2021-02-08 18:43:07 -0600

    Python tests written

--- app.py
+++ app.py
@@ -1,3 +1,49 @@
 from boggle import Boggle
+from flask import Flask, render_template, session, request, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'boggle789'
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def show_game():
+    "Show the game board, the form for making guesses, and the stored high score. High score defaults to 0 if there isn't one."
+    board = boggle_game.make_board()
+    session['board'] = board
+    high_score = session.get('high_score', 0)
+    return render_template('index.html', board=board, high_score=high_score)
+
+@app.route('/guess')
+def validate_guess():
+    "Check the user's submitted guess (passed as a client string) and return JSON with the result of their guess."
+    guess = request.args['guess']
+    result = boggle_game.check_valid_word(session['board'], guess)
+    response = {'result': result}
+    return jsonify(response)
+
+@app.route('/game-stats', methods=["POST"])
+def update_stats():
+    """
+    Increments the number of games this user has played.
+    Check if the score is a new high score.
+    Return JSON that passes back data for the high score.
+    """
+    games_played = session.get('games_played', 0) + 1
+    session['games_played'] = games_played
+
+    score = request.json['score']
+
+    response = {'newHighScore': check_high_score(score)}
+    return jsonify(response)
+
+def check_high_score(score):
+    "Returns true and updates the high score if the score passed is higher than the stored high score. Otherwise returns false."
+    if score > session.get('high_score', 0):
+        session['high_score'] = score
+        return True
+    else:
+        return False
--- test.py
+++ test.py
@@ -1,10 +1,103 @@
 from unittest import TestCase
-from app import app
-from flask import session
+from app import app, check_high_score
+from flask import session, json
 from boggle import Boggle
+
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def test_show_game(self):
+        """
+        Tests that we get a successful response at the '/' route.
+        Also checks that high score is 0 initally, and the same as the session when we have a high score stored there.
+        """
+        with app.test_client() as client:
+            # First test with no previous high score
+            response1 = client.get('/')
+            html1 = response1.get_data(as_text=True)
+            self.assertEqual(response1.status_code, 200)
+            self.assertIn('High score: <span id="high-score">0</span>', html1)
 
+            # Then, test with stored high score
+            with client.session_transaction() as change_session:
+                change_session['high_score'] = 20
+            response2 = client.get('/')
+            html2 = response2.get_data(as_text=True)
+            self.assertIn('High score: <span id="high-score">20</span>', html2)
+
+    def test_validation(self):
+        """
+        Tests that we get the expected JSON response given the query string passed to /guess.
+        Uses a mock board since we can't know which words will be valid on a random board.
+        """
+        mock_board = [
+            ['H', 'E', 'L', 'L', 'O'],
+            ['H', 'E', 'L', 'L', 'O'],
+            ['H', 'E', 'L', 'L', 'O'],
+            ['H', 'E', 'L', 'L', 'O'],
+            ['H', 'E', 'L', 'L', 'O']
+        ]
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session['board'] = mock_board
+            
+            #Check valid guess "hello"
+            response1 = client.get('/guess?guess=hello')
+            json1 = json.loads(response1.get_data(as_text=True))
+            self.assertEqual(response1.status_code, 200)
+            self.assertEqual(response1.content_type, 'application/json')
+            self.assertEqual(json1, {"result": "ok"})
+
+            #Check valid word not on board "the"
+            response2 = client.get('/guess?guess=the')
+            json2 = json.loads(response2.get_data(as_text=True))
+            self.assertEqual(response2.status_code, 200)
+            self.assertEqual(response2.content_type, 'application/json')
+            self.assertEqual(json2, {"result": "not-on-board"})
+
+            #Check invalid word
+            response3 = client.get('/guess?guess=asdfg')
+            json3 = json.loads(response3.get_data(as_text=True))
+            self.assertEqual(response3.status_code, 200)
+            self.assertEqual(response3.content_type, 'application/json')
+            self.assertEqual(json3, {"result": "not-word"})
+
+    def test_stats(self):
+        """
+        Test that the update_stats view function is incrementing the saved 
+        number of games played and returning JSON
+        """
+        with app.test_client() as client:
+            #First check that we save 1 game played if there are no existing games saved in the session.
+            response1 = client.post('/game-stats', json={'score': 20})
+            self.assertEqual(response1.status_code, 200)
+            self.assertEqual(response1.content_type, 'application/json')
+            self.assertEqual(session['games_played'], 1)
+
+            #Next set a number of games played in the session and check that this function increments it
+            with client.session_transaction() as change_session:
+                change_session['games_played'] = 10
+            response2 = client.post('/game-stats', json={'score': 25})
+            self.assertEqual(response2.status_code, 200)
+            self.assertEqual(response2.content_type, 'application/json')
+            self.assertEqual(session['games_played'], 11)
+    
+    def test_high_score(self):
+        """
+        In the context of an HTTP request, runs 3 tests for the high score:
+        1. Returns true and saves our score when we have no saved score and we pass a score > 0
+        2. Returns true and saves a new high score when we have a saved score and we pass a higher score
+        3. Returns false when we have a saved score and we pass a lower score
+        """
+        with app.test_request_context('/'):
+            self.assertTrue(check_high_score(10))
+            self.assertEqual(session['high_score'], 10)
+
+            self.assertTrue(check_high_score(25))
+            self.assertEqual(session['high_score'], 25)
+
+            self.assertFalse(check_high_score(5))
+            self.assertEqual(session['high_score'], 25)
